8f6dae230d43057331b80684ceb92e181a6408bb
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2012-11-28 22:13:27 +0100

    add toolbar

--- src/me/hajo/editor/client/GuiContainer.java
+++ src/me/hajo/editor/client/GuiContainer.java
@@ -1,11 +1,18 @@
 package me.hajo.editor.client;
 
 import me.hajo.editor.client.EntryPoint.StateStorage;
+import me.hajo.editor.client.parts.AddBlockButton;
+import me.hajo.editor.helpers.HajoToolbar;
+import me.hajo.editor.helpers.LinkButton;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteEvent;
+import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteHandler;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.ResizeComposite;
 import com.google.gwt.user.client.ui.Widget;
@@ -19,10 +26,33 @@ public class GuiContainer extends ResizeComposite {
 	@UiField
 	FlowPanel canvas;
 
-	public GuiContainer(HTMLPanel icanvas, String image_upload_url, StateStorage stateStorage) {
-		initWidget(uiBinder.createAndBindUi(this));
-		icanvas.removeFromParent();
-		this.canvas.add(icanvas);
-	}
+	@UiField
+	FlowPanel toolbarContainer;
 
+	HajoToolbar toolbar = new HajoToolbar();
+
+	FlowPanel page;
+
+	public GuiContainer(HTMLPanel ocanvas, String image_upload_url, final StateStorage stateStorage) {
+		initWidget(uiBinder.createAndBindUi(this));
+		toolbarContainer.add(toolbar);
+		ocanvas.removeFromParent();
+		canvas.add(ocanvas);
+		page = new FlowPanel();
+		ocanvas.add(page);
+
+		page.add(new AddBlockButton(page));
+
+		toolbarContainer.add(new LinkButton("icon-save", "Save", "btn-success", new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				stateStorage.sendToServer(new SubmitCompleteHandler() {
+					@Override
+					public void onSubmitComplete(SubmitCompleteEvent event) {
+					}
+				});
+			}
+		}));
+
+	}
 }
